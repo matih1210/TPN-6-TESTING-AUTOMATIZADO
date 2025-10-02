3228297360975f79152567aeab6ccc538ec20801
Nicolás Correa <nicolascorrea1910@gmail.com>
2025-10-02 18:22:21 +0000

    fix: arreglada generación de videos y fotos

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,8 @@ module.exports = defineConfig({
     baseUrl: 'https://thinking-tester-contact-list.herokuapp.com/', // o la URL que te den en la cátedra
     viewportWidth: 1280,
     viewportHeight: 800,
+    video: true,
+    screenshots: true,
+    screenshotOnRunFailure: true,
   },
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,9 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+
+// Hook to take a screenshot after each test
+afterEach(() => {
+  const testName = Cypress.mocha.getRunner().suite.ctx.currentTest.title;
+  cy.screenshot(testName);
+});
